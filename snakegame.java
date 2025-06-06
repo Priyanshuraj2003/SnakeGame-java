95320b29345345cbf2148f3aa3778f37f9a5348b
Priyanshuraj Thakur <priyanshurajthakur114@gmail.com>
2025-06-06 19:47:12 +0530

    Files added

---
+++ snakegame.java
@@ -0,0 +1,19 @@
+
+
+import javax.swing.*;
+
+public class snakegame extends JFrame {
+    
+    snakegame() {
+        super("Snake Game");
+        add(new Board());
+        pack();
+        
+        setLocationRelativeTo(null);
+        setResizable(false);
+    }
+
+    public static void main(String[] args) {
+        new snakegame().setVisible(true);
+    }
+}
